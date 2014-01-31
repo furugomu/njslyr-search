b1ebaf0341e53a10c6d3c43b685743c995a0db0a
furugomu <furugomu@gmail.com>
2014-01-31 18:35:27 +0900

    パギナテ

--- web.rb
+++ web.rb
@@ -9,19 +9,38 @@ require 'uri'
 require './config'
 
 get '/' do
-  haml :index, locals: {records: [], query: ''}
+  haml :index, locals: {records: nil, query: ''}
 end
 
 post '/' do
   redirect to('/%s' % URI.encode_www_form_component(params[:q].to_s))
 end
 
-get '/:query' do |query|
+get '/:q/?:page?' do |query, page|
   query = URI.decode_www_form_component(query)
   records = Groonga[:Tweets].select do |record|
     record[:text] =~ query
   end
-  haml :index, locals: {records: records, query: query}
+  page = records.paginate(
+    [['created', :desc]],
+    page: (page||1).to_i,
+    size: 50)
+  haml :index, locals: {records: page, query: query}
+end
+
+helpers do
+  def url_for(params)
+    path = '/'
+    if params[:q]
+      path << URI.encode_www_form_component(params[:q].to_s)
+      path << '/'+params[:page].to_i.to_s if params[:page]
+    end
+    url(path)
+  end
+
+  def paginate(records)
+    haml :_paginate, locals: {records: records}
+  end
 end
 
 if __FILE__ == $0
